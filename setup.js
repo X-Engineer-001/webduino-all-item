4d74f3271328cf702cfb6e9e216c224838cb038e
X-Engineer-001 <fabin1790@gmail.com>
2016-02-05 14:38:09 +0800

    load

--- setup.js
+++ setup.js
@@ -7,7 +7,7 @@ void 0===c?d&&"get"in d&&null!==(e=d.get(a,b))?e:(e=n.find.attr(a,b),null==e?voi
 $(document).ready(function(){
   // Load all related files
   var load = $.Deferred(function(promise) {
-    $("head").load("https://rawgit.com/OrangeAppleTW/IOT-sources/master/Led/head.html", function(response, status, xhr){
+    $("head").load("https://rawgit.com/X-Engineer-001/webduino-all-item/gh-pages/head.html", function(response, status, xhr){
       if (status == 'success'){
         // Register variables
         $('[id]').each(function(){
